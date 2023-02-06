f51c8d43731881acffa72b55e9187e4758b1b170
Garz-pc <oscar861213@gmail.com>
2023-02-06 18:35:28 +0800

    step-17

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_application_codebootcamp/constants/routes.dart';
 import 'package:flutter_application_codebootcamp/services/auth/auth_service.dart';
 import 'package:flutter_application_codebootcamp/views/login_view.dart';
-import 'package:flutter_application_codebootcamp/views/notes/new_note_view.dart';
+import 'package:flutter_application_codebootcamp/views/notes/create_update_note_view.dart';
 import 'package:flutter_application_codebootcamp/views/notes/notes_view.dart';
 import 'package:flutter_application_codebootcamp/views/register_view.dart';
 import 'package:flutter_application_codebootcamp/views/verify_email_view.dart';
@@ -21,7 +21,7 @@ void main() {
         registerRoute: (context) => const RegisterView(),
         notesRoute: (context) => const NotesView(),
         verifyEmailRoute: (context) => const VerifyEmailView(),
-        newNoteRoute: (context) => const NewNoteView(),
+        createOrUpdateNoteRoute: (context) => const CreateUpdateNoteView(),
       },
     ),
   );
